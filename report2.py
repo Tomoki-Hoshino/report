f433f3fb959a2fac1950bec13ec8b21d33c09e8b
Tomoki-Hoshino <tomokihoshino0607@keio.jp>
2025-07-22 10:23:14 +0900

    add 2

---
+++ report2.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize_scalar
+
+def util(cons,gamma):
+  return max(cons, 0.0001)**(1.0-gamma)/(1.0-gamma)
+
+#parameters
+gamma = 2.0
+beta = 0.985**20
+r = 1.025**20-1.0
+jj = 60
+l = np.array([0.8027, 1.0, 1.2457])
+NL = 3
+prob = np.array([
+    [0.7451, 0.2528, 0.0021],
+    [0.1360, 0.7281, 0.1361],
+    [0.0021, 0.2528, 0.7451]
+])
+mu_1 = np.array([1.0/NL,1.0/NL,1.0/NL])
+
+mu_2 = np.zeros(NL)
+
+for il in range(NL):
+    for ilp in range(NL):
+        mu_2[ilp] += prob[il,ilp]*mu_1[il]
+
+middle_age = range(20, 40)
+tax_rate = 0.3
+tax_revenue = 0.0
+for ij in middle_age:
+  for il in range(NL):
+    for ia in range(NA):
+      tax_revenue += tax_rate * l[il] * mu_2[il]
+
+print(tax_revenue * len(middle_age))
